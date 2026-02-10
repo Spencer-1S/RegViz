0eff82ac3c2e0496cc4c119345178196e26eb52a
vishal <vishal98450anand@gmail.com>
2026-02-11 01:49:01 +0530

    deployment configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/regextodfa/', // TODO: Change this to your repository name
+  base: '/RegViz/',
   plugins: [react()],
   server: {
     port: 5173,
